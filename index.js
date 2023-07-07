ee5e7213da817086928aeb8c6aefc7c89516ceb0
RaziaAkbari <akbariharis14@gmail.com>
2023-07-07 13:24:42 +0430

    initial commits

---
+++ index.js
@@ -0,0 +1,37 @@
+// Wait for the page to finish loading
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the reference to the website element
+  const websiteElement = document.getElementById("h1")
+  const text = "MY NAME IS RAZIA"
+  let currentIndex = 0
+  let isDeleting = false
+
+  // Function to type and delete the text
+  function typeAndDelete() {
+    const currentText = websiteElement.innerHTML
+
+    if (!isDeleting) {
+      websiteElement.innerHTML = currentText + text[currentIndex]
+      currentIndex++
+
+      if (currentIndex === text.length) {
+        isDeleting = true
+        setTimeout(typeAndDelete, 2000)
+        return
+      }
+    } else {
+      websiteElement.innerHTML = currentText.slice(0, -1)
+
+      if (currentText.length === 0) {
+        isDeleting = false
+        currentIndex = 0
+      }
+    }
+
+    const typingSpeed = isDeleting ? 100 : 300
+    setTimeout(typeAndDelete, typingSpeed)
+  }
+
+  // Start typing and deleting
+  typeAndDelete()
+})
